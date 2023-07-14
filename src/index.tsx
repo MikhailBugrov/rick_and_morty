0c081eb27cb17a10646fc1e28a940e3388edcd6e
Mikhail Bugrov <mikhail.bugrov.v@gmail.com>
2023-07-15 00:24:50 +0300

    setup gh-pages

--- src/index.tsx
+++ src/index.tsx
@@ -3,22 +3,26 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import App from './App';
 import { store } from './store/configureStore';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 import { Box, ThemeProvider, CssBaseline } from '@mui/material';
 import theme from './theme';
+
+//I am using HashRouter instead of BrowserRouter due to the peculiarities of GitHub Pages hosting.
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      {/* <BrowserRouter> */}
+      <HashRouter> 
         <Box sx={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
           <ThemeProvider theme={theme}>
             <CssBaseline />
             <App />
           </ThemeProvider>
         </Box>
-      </BrowserRouter>
+      </HashRouter>
+      {/* </BrowserRouter> */}
     </Provider>
   </React.StrictMode>
 );
